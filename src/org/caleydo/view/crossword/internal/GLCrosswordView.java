35aac3bbba83b91ae1b8ee90175759b837284842
Samuel Gratzl <samuel_gratzl@gmx.at>
2013-07-04 17:06:33 +0200

    update source headers

--- src/org/caleydo/view/crossword/internal/GLCrosswordView.java
+++ src/org/caleydo/view/crossword/internal/GLCrosswordView.java
@@ -1,19 +1,8 @@
 /*******************************************************************************
- * Caleydo - visualization for molecular biology - http://caleydo.org
- *
- * Copyright(C) 2005, 2012 Graz University of Technology, Marc Streit, Alexander Lex, Christian Partl, Johannes Kepler
- * University Linz </p>
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program. If not, see
- * <http://www.gnu.org/licenses/>
- *******************************************************************************/
+ * Caleydo - Visualization for Molecular Biology - http://caleydo.org
+ * Copyright (c) The Caleydo Team. All rights reserved.
+ * Licensed under the new BSD license, available at http://caleydo.org/license
+ ******************************************************************************/
 package org.caleydo.view.crossword.internal;
 
 import java.util.Iterator;
@@ -34,9 +23,9 @@ import com.google.common.collect.Iterators;
 
 /**
  * basic view based on {@link GLElement} with a {@link AMultiTablePerspectiveElementView}
- * 
+ *
  * @author Samuel Gratzl
- * 
+ *
  */
 public class GLCrosswordView extends AMultiTablePerspectiveElementView {
 	public static final String VIEW_TYPE = "org.caleydo.view.crossword";
@@ -73,7 +62,11 @@ public class GLCrosswordView extends AMultiTablePerspectiveElementView {
 					it.remove();
 			}
 		}
-		for (TablePerspective t : added)
+		for (TablePerspective t : added) {
 			crossword.add(new CrosswordElement(t));
+			for (TablePerspective rt : t.getRecordSubTablePerspectives())
+				crossword.add(new CrosswordElement(rt));
+		}
+
 	}
 }
